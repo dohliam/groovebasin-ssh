ae555fd3030983b8e972f48d4394ecd24fb89e59
dohliam <dohliam@gmail.com>
2015-08-05 15:01:54 -0700

    add option to remove db lock

--- gb.sh
+++ gb.sh
@@ -3,11 +3,14 @@
 IP="localhost"	# remote IP address
 GB_LOC=".groovebasin"	# location of the remote groovebasin folder
 
-# forward ports to groovebasin and then start the groovebasin server
-
 echo "Connecting to remote IP: $IP"
-ssh -t -t -L 16242:127.0.0.1:16242 $IP "cd $GB_LOC; groovebasin" &
+
+# forward ports to groovebasin and then start the groovebasin server
+if [ "$1" == '' ]; then
+  ssh -t -t -L 16242:127.0.0.1:16242 $IP "cd $GB_LOC; groovebasin" &
+elif [ $1 == "-k" ]; then
+  ssh -t -t -L 16242:127.0.0.1:16242 $IP "killall nodejs; cd $GB_LOC; groovebasin" &
+fi
 
 # open browser to groovebasin page
-
 xdg-open http://0.0.0.0:16242/
